912e10641e3434546d991da6c93e1b82378fbfae
spiyse <daniels@spiyse.dev>
2026-02-18 12:56:53 +0200

    yessking

--- config.php
+++ config.php
@@ -4,9 +4,9 @@ return [
     "database" => [
         "host" => "localhost",
         "port" => 3306,
-        "user" => "root",
-        "password" => "",
-        "dbname" => "blog",
+        "user" => "projekts_blogthingy",
+        "password" => "blogythingy3452",
+        "dbname" => "projekts_blogthingy",
         "charset" => "utf8mb4"
     ],
     "email" => [],
